77974664182017f17cf669f6bd485978e7692357
bookworkingnet <121592024+bookworkingnet@users.noreply.github.com>
2023-11-09 10:50:58 +0700

    Add files via upload

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,24 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build.Content;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
-    [SerializeField] float loadDelay = 0.5f;
-    [SerializeField] ParticleSystem finishEffect;
-    void OnTriggerEnter2D(Collider2D other) 
-    {
-        if(other.tag == "Player")
-        {
-            finishEffect.Play();
-            GetComponent<AudioSource>().Play();
-            Invoke("ReloadScene", loadDelay);
-        }
-    }
+    public static int sceneIndex = 0; // khai báo sceneIndex ở ngoài
 
-    void ReloadScene()
+  [SerializeField] float loadDelay = 0.5f;
+  [SerializeField] ParticleSystem finishEffect;
+
+  
+  
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    if(other.tag == "Player")
     {
-        SceneManager.LoadScene(0);
+      finishEffect.Play();
+      GetComponent<AudioSource>().Play();
+      Invoke("ReloadScene", loadDelay); 
     }
+  }
+
+  void ReloadScene()
+  {   
+    
+    sceneIndex++; // tăng sceneIndex lên 1
+    if(sceneIndex > 2) {
+      Debug.Log("Man Cuoi");
+      return ;
+    }
+    SceneManager.LoadScene(sceneIndex); 
+    
+    
+  }
+
 }
